03064501b1b1510ab7e73b7ebf2bca079d51aac0
seolleung2 <seolleung22@gmail.com>
2022-11-21 23:56:49 +0900

    add: 메인페이지 설정, 더미데이터 및 이미지 추가

---
+++ pages/events/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AllEventsPage() {
+  return (
+    <div>
+      <h1>AllEventsPage</h1>
+    </div>
+  );
+}
+
+export default AllEventsPage;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,13 @@
 import React from "react";
 import Link from "next/link";
+import { getFeaturedEvents } from "../dummy-data";
 
 export default function Home() {
+  const featuredEvents = getFeaturedEvents();
+  console.log(featuredEvents);
   return (
     <div>
       <h1>The Home Page</h1>
-      <ul>
-        <li>
-          <Link href="/portfolio">Portfolio</Link>
-        </li>
-        <li>
-          <Link href="clients">Clients</Link>
-        </li>
-      </ul>
     </div>
   );
 }
