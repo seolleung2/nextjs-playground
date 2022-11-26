0638224c66ddde0cb0692bf82cdcd9a45330f1e0
seolleung2 <seolleung22@gmail.com>
2022-11-26 15:17:24 +0900

    fix: MextJS - styled-components 렌더링 이슈 초기설정 수정 - yarn add babel-plugin-styled-components 설치 후 .babelrc 설정 - _document.tsx 파일 초기설정, 타입 수정 변경 - 초기 렌더링 시 html 을 먼저 불러와 깜빡이는 현상 fix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,18 +1,54 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import React, { ReactElement } from "react";
+import Document, {
+  Head,
+  Html,
+  Main,
+  NextScript,
+  DocumentInitialProps,
+  DocumentContext,
+} from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
-export default function Document() {
-  return (
-    <Html>
-      <Head>
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Open+Sans:wght@400;700&display=swap"
-        />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App: React.ComponentType<any>) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+          enhanceComponent: (Component) => Component,
+        });
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: [initialProps.styles, sheet.getStyleElement()],
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render(): ReactElement {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&family=Open+Sans:wght@400;700&display=swap"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
