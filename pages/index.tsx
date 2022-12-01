2de490ec2292ea1aac02a02bfb5f21b34e617074
seolleung2 <seolleung22@gmail.com>
2022-12-01 23:51:21 +0900

    fix: getStaticProps 를 사용한 pre-fetching-data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,21 @@
 import React from "react";
-import { getFeaturedEvents } from "@Data/dummy-data";
 import { EventList } from "@Components/events";
 
-export default function Home() {
-  const featuredEvents = getFeaturedEvents();
+export interface IEventDataProps {
+  id: string;
+  title: string;
+  description: string;
+  location: string;
+  date: string;
+  image: string;
+  isFeatured: boolean;
+}
 
+interface HomeProps {
+  featuredEvents: IEventDataProps[];
+}
+
+export default function Home({ featuredEvents }: HomeProps) {
   return (
     <div>
       <EventList items={featuredEvents} />
@@ -12,4 +23,25 @@ export default function Home() {
   );
 }
 
-// getStaticProps
+export async function getStaticProps() {
+  return fetch(
+    "https://nextjs-course-df954-default-rtdb.firebaseio.com/events.json"
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const dataSet = [];
+
+      for (const key in data) {
+        dataSet.push({
+          ...data[key],
+          id: key,
+        });
+      }
+
+      return {
+        props: {
+          featuredEvents: dataSet.filter((data) => data.isFeatured),
+        },
+      };
+    });
+}
